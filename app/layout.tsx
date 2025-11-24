4cdb7f3880f7c7741ebcf168dcad57f10f1b1aad
Lixdevv <jorgeleython2@gmail.com>
2025-11-24 13:57:16 -0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "CHOG Agent",
+  title: "Monad Agent",
   description: "Token analyzer powered by CHOG",
   icons: {
     icon: "https://i.postimg.cc/rFpnY7sc/fotiyo-(1).png",
