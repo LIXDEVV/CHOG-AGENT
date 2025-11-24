b209b9a0cdb7076adbfa90280066b18af2179f3d
Lixdevv <jorgeleython2@gmail.com>
2025-11-24 10:39:21 -0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="https://i.postimg.cc/rFpnY7sc/fotiyo-(1).png" />
+        <link rel="apple-touch-icon" href="https://i.postimg.cc/rFpnY7sc/fotiyo-(1).png" />
       </head>
       <body className={`font-sans antialiased`}>{children}</body>
     </html>
